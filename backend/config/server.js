53de23aafe5c0bd63d795bf0baf0097a75c57073
Joshua Julius <joshjulius1@gmail.com>
2021-04-02 15:52:11 -0500

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b4fe20672fc4d580521f746797b2b22d'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '99b114cb-9dcb-477d-a866-5a44323a1a67'
+};
